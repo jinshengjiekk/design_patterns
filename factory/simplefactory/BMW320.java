f7b5afc7f3d84f172f8f56d5c8648de51f33b0cd
Hello King <jinshengjiekk@163.com>
2016-03-14 21:22:20 +0800

    工厂模式

---
+++ factory/simplefactory/BMW320.java
@@ -0,0 +1,9 @@
+package com.jsj.designpatterns.factory.simplefactory;
+//2016年3月14日下午8:25:15
+public class BMW320 extends BMW {
+
+	public BMW320() {
+		System.out.println("生产制造BMW320!");
+	}
+
+}
---
+++ factory/simplefactory/BMW523.java
@@ -0,0 +1,9 @@
+package com.jsj.designpatterns.factory.simplefactory;
+//2016年3月14日下午8:26:09
+public class BMW523  extends BMW{
+
+	public BMW523() {
+		System.out.println("生产制造BMW523!");
+	}
+
+}
