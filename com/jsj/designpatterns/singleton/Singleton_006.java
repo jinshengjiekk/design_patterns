5aae495006754e843c9b604fb07e760c867612c9
jinshengjie(Mac) <jinshengjiekk@163.com>
2017-03-08 23:43:50 +0800

    add singleton_006

---
+++ com/jsj/designpatterns/singleton/Singleton_006.java
@@ -0,0 +1,11 @@
+package com.jsj.designpatterns.singleton;
+
+/**
+ * Created by jinshengjie on 2017/3/8.
+ *
+ * @see <a href= "http://stackoverflow.com/questions/26285520/implementing-singleton-with-an-enum-in-java">
+ *     stackoverflow-->implementing-singleton-with-an-enum-in-java</a>
+ */
+public enum Singleton_006 {
+    INSTANCE;
+}
